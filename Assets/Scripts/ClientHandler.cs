1690c539b42f79c5283f58d755383d99aa587cbb
Maxime <maximeld19@gmail.com>
2020-10-28 11:07:19 +0100

    Added Scripts from last version

---
+++ Assets/Scripts/ClientHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class ClientHandle : MonoBehaviour
+{
+    public static void Welcome(Packet _packet)
+    {
+        string _msg = _packet.ReadString();
+        int _myId = _packet.ReadInt();
+
+        Debug.Log($"Message from server: {_msg}");
+        Client.instance.myId = _myId;
+        ClientSend.WelcomeReceived();
+
+        // Now that we have the client's id, connect UDP
+        Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
+    }
+
+    public static void GetFrame(Packet packet)
+    {
+
+        var nbBytes = packet.ReadInt();
+        var bytes = packet.ReadBytes(nbBytes);
+        VideoManager.instance.ChangeImage(bytes);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Vector2 position = new Vector2();
+
+    private void FixedUpdate()
+    {
+        SendInputToServer();
+        SendLightControlToServer();
+    }
+
+    /// <summary>Sends player input to the server.</summary>
+    private void SendInputToServer()
+    {
+        position = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        ClientSend.PlayerMovement(position);
+    }
+    
+    private void SendLightControlToServer()
+    {
+        if (!Input.GetButtonDown("Light")) return;
+        ClientSend.LightControl();
+    }
+
+    public Vector2 GetPosition()
+    {
+        return position;
+    }
+}
